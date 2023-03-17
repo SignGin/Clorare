98babd3c70c8ca46a5ad74781048849183099241
rlguswn <0318joo@naver.com>
2023-03-17 17:31:27 +0900

    모델 수정 및 키 파일 분리

--- server/restapi/views.py
+++ server/restapi/views.py
@@ -1,15 +1,36 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets, permissions
-from restapi.serializers import UserSerializer, GroupSerializer
+from django.contrib.auth.models import User
+from requests import Response
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from restapi.serializers import ClothesSerializer
+from weatherdata.models import Clothes
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from weatherdata.api import get_weather
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+# class AnalysisViewSet(viewsets.ModelViewSet):
+#     queryset = Analysis.objects.all()
+#     serializer_class = AnalysisSerializer
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+class ClothesViewSet(viewsets.ModelViewSet):
+    queryset = Clothes.objects.all()
+    serializer_class = ClothesSerializer
+
+
+class Recommendation(viewsets.ModelViewSet):
+    w_data = dict()
+    get_weather(w_data)
+    queryset = Clothes.objects.all()
+    serializer_class = ClothesSerializer
+
+    def classification(self, request):
+        queryset = self.get_queryset()
+        serializer = ClothesSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.accounts.all()

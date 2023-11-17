8dd795e30fb3f13edd3512fe323466785c1fbb5a
주기현 <0318joo@naver.com>
2023-11-17 20:06:13 +0900

    cloth_type과 gender의 입력값 정형화

--- server/clothes/models.py
+++ server/clothes/models.py
@@ -2,11 +2,21 @@ from django.db import models
 
 
 class Clothes(models.Model):
+    CLOTH_TYPES = [
+        ('top', '상의'),
+        ('bottom', '하의'),
+        ('coat', '외투')
+    ]
+    GENDER = [
+        ('female', '여자'),
+        ('male', '남자'),
+        ('unisex', '공용')
+    ]
     category = models.CharField(max_length=10)  # ex) 상의, 하의
-    cloth_type = models.CharField(max_length=10)  # ex) 티셔츠, 니트
+    cloth_type = models.CharField(max_length=10, choices=CLOTH_TYPES)  # ex) 티셔츠, 니트
     max_temp = models.IntegerField()
     min_temp = models.IntegerField()
-    gender = models.CharField(max_length=6)  # 여성, 남성, 무관
+    gender = models.CharField(max_length=6, choices=GENDER)  # 여성, 남성, 무관
     image = models.ImageField(upload_to='clothes', null=True, blank=True)
 
     def __str__(self):

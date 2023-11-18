09933175778eac1d5ea9aac70d75868a6100c980
주기현 <0318joo@naver.com>
2023-11-18 22:15:35 +0900

    모델 choice 오타 수정

--- server/clothes/models.py
+++ server/clothes/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 
 class Clothes(models.Model):
-    CLOTH_TYPES = [
+    CATEGORY = [
         ('top', '상의'),
         ('bottom', '하의'),
         ('coat', '외투')
     ]
     GENDER = [
-        ('female', '여자'),
-        ('male', '남자'),
-        ('unisex', '공용')
+        ('0', '여자'),
+        ('1', '남자'),
+        ('2', '공용')
     ]
-    category = models.CharField(max_length=10)  # ex) 상의, 하의
-    cloth_type = models.CharField(max_length=10, choices=CLOTH_TYPES)  # ex) 티셔츠, 니트
+    category = models.CharField(max_length=10, choices=CATEGORY)  # ex) 상의, 하의
+    cloth_type = models.CharField(max_length=10)  # ex) 티셔츠, 니트
     max_temp = models.IntegerField()
     min_temp = models.IntegerField()
     gender = models.CharField(max_length=6, choices=GENDER)  # 여성, 남성, 무관

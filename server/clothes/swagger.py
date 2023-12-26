65f31aace49d1508f3f7998ecccd624f5865f5e9
주기현 <0318joo@naver.com>
2023-12-26 15:18:20 +0900

    스웨거 파라미터와 스키마 파일 분리

---
+++ server/clothes/swagger.py
@@ -0,0 +1,45 @@
+from drf_yasg import openapi
+
+# Request Parameter
+rp_id = openapi.Parameter('id', openapi.IN_PATH, type=openapi.TYPE_INTEGER,
+                          description='cloth id')
+rp_gender = openapi.Parameter('gender', openapi.IN_PATH, type=openapi.TYPE_INTEGER, enum=[0, 1, 2],
+                              description='gender to wear cloth\nfemale=0\nmale=1\nunisex=2')
+
+# Cloth Model Schema
+cms_id = openapi.Schema(type=openapi.TYPE_INTEGER,
+                        description='cloth id')
+cms_category = openapi.Schema(type=openapi.TYPE_STRING, enum=['top', 'bottom', 'coat'],
+                              description='cloth category\nmax_length:6')
+cms_cloth_type = openapi.Schema(type=openapi.TYPE_STRING,
+                                description='cloth name\nex) t-shirts, sweater, jeans')
+cms_season = openapi.Schema(type=openapi.TYPE_STRING, enum=['spring', 'summer', 'autumn', 'winter'],
+                            description='season to wear')
+cms_gender = openapi.Schema(type=openapi.TYPE_STRING, enum=['female', 'male', 'unisex'],
+                            description='gender to wear cloth')
+cms_image = openapi.Schema(type=openapi.TYPE_FILE,
+                           description='cloth image')
+
+# Weather Model Schema
+wms_weather = openapi.Schema(type=openapi.TYPE_STRING,
+                             description='weather when you request')
+wms_temperature = openapi.Schema(type=openapi.TYPE_NUMBER,
+                                 description='temperature when you request')
+wms_feels_like = openapi.Schema(type=openapi.TYPE_NUMBER,
+                                description='feels like temperature when you request')
+wms_daily_high = openapi.Schema(type=openapi.TYPE_NUMBER,
+                                description='daily high temperature when you request')
+wms_daily_low = openapi.Schema(type=openapi.TYPE_NUMBER,
+                               description='daily low temperature when you request')
+wms_humidity = openapi.Schema(type=openapi.TYPE_INTEGER,
+                              description='humidity when you request')
+wms_wind_speed = openapi.Schema(type=openapi.TYPE_NUMBER,
+                                description='wind speed when you request')
+
+# System Message Schema
+sms_202 = openapi.Schema(type=openapi.TYPE_STRING,
+                         description='202 accepted message')
+sms_400 = openapi.Schema(type=openapi.TYPE_STRING,
+                         description='400 error message')
+sms_500 = openapi.Schema(type=openapi.TYPE_STRING,
+                         description='500 error message')
--- server/clothes/views.py
+++ server/clothes/views.py
@@ -6,54 +6,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Clothes
 from .serializers import ClothesSerializer, ClorareSerializer
+from . import swagger as sw
 
 p = os.path.abspath('.') + r'\openweather'
 sys.path.insert(1, p)
 from openweatherapi import open_weather_api
 
-# Request Parameter
-rp_id = openapi.Parameter('id', openapi.IN_PATH, type=openapi.TYPE_INTEGER,
-                          description='cloth id')
-rp_gender = openapi.Parameter('gender', openapi.IN_PATH, type=openapi.TYPE_INTEGER, enum=[0, 1, 2],
-                              description='gender to wear cloth\nfemale=0\nmale=1\nunisex=2')
-
-# Cloth Model Schema
-cms_id = openapi.Schema(type=openapi.TYPE_INTEGER,
-                        description='cloth id')
-cms_category = openapi.Schema(type=openapi.TYPE_STRING, enum=['top', 'bottom', 'coat'],
-                              description='cloth category\nmax_length:6')
-cms_cloth_type = openapi.Schema(type=openapi.TYPE_STRING,
-                                description='cloth name\nex) t-shirts, sweater, jeans')
-cms_season = openapi.Schema(type=openapi.TYPE_STRING, enum=['spring', 'summer', 'autumn', 'winter'],
-                            description='season to wear')
-cms_gender = openapi.Schema(type=openapi.TYPE_STRING, enum=['female', 'male', 'unisex'],
-                            description='gender to wear cloth')
-cms_image = openapi.Schema(type=openapi.TYPE_FILE,
-                           description='cloth image')
-
-# Weather Model Schema
-wms_weather = openapi.Schema(type=openapi.TYPE_STRING,
-                             description='weather when you request')
-wms_temperature = openapi.Schema(type=openapi.TYPE_NUMBER,
-                                 description='temperature when you request')
-wms_feels_like = openapi.Schema(type=openapi.TYPE_NUMBER,
-                                description='feels like temperature when you request')
-wms_daily_high = openapi.Schema(type=openapi.TYPE_NUMBER,
-                                description='daily high temperature when you request')
-wms_daily_low = openapi.Schema(type=openapi.TYPE_NUMBER,
-                               description='daily low temperature when you request')
-wms_humidity = openapi.Schema(type=openapi.TYPE_INTEGER,
-                              description='humidity when you request')
-wms_wind_speed = openapi.Schema(type=openapi.TYPE_NUMBER,
-                                description='wind speed when you request')
-
-# System Message Schema
-sms_202 = openapi.Schema(type=openapi.TYPE_STRING,
-                         description='202 accepted message')
-sms_400 = openapi.Schema(type=openapi.TYPE_STRING,
-                         description='400 error message')
-sms_500 = openapi.Schema(type=openapi.TYPE_STRING,
-                         description='500 error message')
 
 # Create your views here.
 class ClothesView(APIView):
@@ -63,12 +21,12 @@ class ClothesView(APIView):
         responses={
             status.HTTP_200_OK: openapi.Response(
                 'Success', schema=openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                    'id': cms_id,
-                    'category': cms_category,
-                    'cloth_type': cms_cloth_type,
-                    'season': cms_season,
-                    'gender': cms_gender,
-                    'image': cms_image
+                    'id': sw.cms_id,
+                    'category': sw.cms_category,
+                    'cloth_type': sw.cms_cloth_type,
+                    'season': sw.cms_season,
+                    'gender': sw.cms_gender,
+                    'image': sw.cms_image
                 })
             )
         }
@@ -84,28 +42,28 @@ class ClothesView(APIView):
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
             properties={
-                'category': cms_category,
-                'cloth_type': cms_cloth_type,
-                'season': cms_season,
-                'gender': cms_gender,
-                'image': cms_image
+                'category': sw.cms_category,
+                'cloth_type': sw.cms_cloth_type,
+                'season': sw.cms_season,
+                'gender': sw.cms_gender,
+                'image': sw.cms_image
             },
             required=['category', 'cloth_type', 'season', 'gender']
         ),
         responses={
             status.HTTP_201_CREATED: openapi.Response(
                 'Success', schema=openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                    'id': cms_id,
-                    'category': cms_category,
-                    'cloth_type': cms_cloth_type,
-                    'season': cms_season,
-                    'gender': cms_gender,
-                    'image': cms_image
+                    'id': sw.cms_id,
+                    'category': sw.cms_category,
+                    'cloth_type': sw.cms_cloth_type,
+                    'season': sw.cms_season,
+                    'gender': sw.cms_gender,
+                    'image': sw.cms_image
                 })
             ),
             status.HTTP_400_BAD_REQUEST: openapi.Response(
                 'Fail', schema=openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                    'message': sms_400
+                    'message': sw.sms_400
                 })
             )
         }
@@ -122,21 +80,21 @@ class ClothesDetailView(APIView):
     @swagger_auto_schema(
         operation_id='Detail cloth data',
         operation_description='Request cloth data by selecting cloth id number',
-        manual_parameters=[rp_id],
+        manual_parameters=[sw.rp_id],
         responses={
             status.HTTP_200_OK: openapi.Response(
                 'Success', schema=openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                    'id': cms_id,
-                    'category': cms_category,
-                    'cloth_type': cms_cloth_type,
-                    'season': cms_season,
-                    'gender': cms_gender,
-                    'image': cms_image
+                    'id': sw.cms_id,
+                    'category': sw.cms_category,
+                    'cloth_type': sw.cms_cloth_type,
+                    'season': sw.cms_season,
+                    'gender': sw.cms_gender,
+                    'image': sw.cms_image
                 })
             ),
             status.HTTP_500_INTERNAL_SERVER_ERROR: openapi.Response(
                 'Success', schema=openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                    'message': sms_500
+                    'message': sw.sms_500
                 })
             )
         }
@@ -149,37 +107,37 @@ class ClothesDetailView(APIView):
     @swagger_auto_schema(
         operation_id='Update cloth data',
         operation_description='Update user custom cloth data',
-        manual_parameters=[rp_id],
+        manual_parameters=[sw.rp_id],
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
             properties={
-                'category': cms_category,
-                'cloth_type': cms_cloth_type,
-                'season': cms_season,
-                'gender': cms_gender,
-                'image': cms_image
+                'category': sw.cms_category,
+                'cloth_type': sw.cms_cloth_type,
+                'season': sw.cms_season,
+                'gender': sw.cms_gender,
+                'image': sw.cms_image
             },
             required=[]
         ),
         responses={
             status.HTTP_202_ACCEPTED: openapi.Response(
                 'Success', schema=openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                    'id': cms_id,
-                    'category': cms_category,
-                    'cloth_type': cms_cloth_type,
-                    'season': cms_season,
-                    'gender': cms_gender,
-                    'image': cms_image
+                    'id': sw.cms_id,
+                    'category': sw.cms_category,
+                    'cloth_type': sw.cms_cloth_type,
+                    'season': sw.cms_season,
+                    'gender': sw.cms_gender,
+                    'image': sw.cms_image
                 })
             ),
             status.HTTP_400_BAD_REQUEST: openapi.Response(
                 'Fail', schema=openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                    'message': sms_400
+                    'message': sw.sms_400
                 })
             ),
             status.HTTP_500_INTERNAL_SERVER_ERROR: openapi.Response(
                 'Success', schema=openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                    'message': sms_500
+                    'message': sw.sms_500
                 })
             )
         }
@@ -195,16 +153,16 @@ class ClothesDetailView(APIView):
     @swagger_auto_schema(
         operation_id='Delete cloth data',
         operation_description='Delete cloth data by selecting cloth id number',
-        manual_parameters=[rp_id],
+        manual_parameters=[sw.rp_id],
         responses={
             status.HTTP_202_ACCEPTED: openapi.Response(
                 'Success', schema=openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                    'message': sms_202
+                    'message': sw.sms_202
                 })
             ),
             status.HTTP_500_INTERNAL_SERVER_ERROR: openapi.Response(
                 'Success', schema=openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                    'message': sms_500
+                    'message': sw.sms_500
                 })
             )
         }
@@ -219,40 +177,40 @@ class ClothesRecommendationView(APIView):
     @swagger_auto_schema(
         operation_id='Cloth recommendation',
         operation_description='Recommend clothes at random',
-        manual_parameters=[rp_gender],
+        manual_parameters=[sw.rp_gender],
         responses={
             status.HTTP_200_OK: openapi.Response(
                 'Success', schema=openapi.Schema(type=openapi.TYPE_OBJECT, properties={
                     'top': openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                        'category': cms_category,
-                        'cloth_type': cms_cloth_type,
-                        'season': cms_season,
-                        'gender': cms_gender,
-                        'image': cms_image
+                        'category': sw.cms_category,
+                        'cloth_type': sw.cms_cloth_type,
+                        'season': sw.cms_season,
+                        'gender': sw.cms_gender,
+                        'image': sw.cms_image
                     }),
                     'bot': openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                        'category': cms_category,
-                        'cloth_type': cms_cloth_type,
-                        'season': cms_season,
-                        'gender': cms_gender,
-                        'image': cms_image
+                        'category': sw.cms_category,
+                        'cloth_type': sw.cms_cloth_type,
+                        'season': sw.cms_season,
+                        'gender': sw.cms_gender,
+                        'image': sw.cms_image
                     }),
                     'coat': openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                        'category': cms_category,
-                        'cloth_type': cms_cloth_type,
-                        'season': cms_season,
-                        'gender': cms_gender,
-                        'image': cms_image
+                        'category': sw.cms_category,
+                        'cloth_type': sw.cms_cloth_type,
+                        'season': sw.cms_season,
+                        'gender': sw.cms_gender,
+                        'image': sw.cms_image
                     }),
-                    'gender': cms_gender,
+                    'gender': sw.cms_gender,
                     'w_data': openapi.Schema(type=openapi.TYPE_OBJECT, properties={
-                        'weather': wms_weather,
-                        'temperature': wms_temperature,
-                        'feels_like': wms_feels_like,
-                        'daily_high': wms_daily_high,
-                        'daily_low': wms_daily_low,
-                        'humidity': wms_humidity,
-                        'wind_speed': wms_wind_speed
+                        'weather': sw.wms_weather,
+                        'temperature': sw.wms_temperature,
+                        'feels_like': sw.wms_feels_like,
+                        'daily_high': sw.wms_daily_high,
+                        'daily_low': sw.wms_daily_low,
+                        'humidity': sw.wms_humidity,
+                        'wind_speed': sw.wms_wind_speed
                     })
                 })
             )

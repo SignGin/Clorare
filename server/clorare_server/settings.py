e61447e938fd96d7aac7eadd52595df84285b306
주기현 <0318joo@naver.com>
2023-12-26 19:37:28 +0900

    csrf 설정 변경

--- server/clorare_server/settings.py
+++ server/clorare_server/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -57,10 +57,22 @@ MIDDLEWARE = [
 # DJANGO-CORS-HEADERS
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 # CSRF
-# CSRF_TRUSTED_ORIGINS = [
-#     'http://localhost:3000'
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000'
+]
 
 ROOT_URLCONF = 'clorare_server.urls'
 

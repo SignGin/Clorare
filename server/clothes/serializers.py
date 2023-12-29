8a059a85a051e98c09fdfc8cc6ca9effa6d99ef4
주기현 <0318joo@naver.com>
2023-12-29 22:46:25 +0900

    base64 인코딩 파일 저장용 디코딩 함수 추가

--- server/clothes/serializers.py
+++ server/clothes/serializers.py
@@ -1,5 +1,24 @@
+from django.conf import settings
 from rest_framework import serializers
 from .models import Clothes
+import base64
+
+
+def dec_b64_img(dict_data):
+    if dict_data['image']:
+        header, data = dict_data['image'].split(';base64,')
+        data_format, ext = header.split('/')
+
+        image_path = dict_data['season']
+        image_name = dict_data['cloth_type']
+        dec_data = base64.b64decode(data)
+        image_root = f'{settings.MEDIA_ROOT}\\clothes\\{image_name}.{ext}'
+
+        with open(image_root, 'wb') as f:
+            f.write(dec_data)
+
+        return f'{image_name}.{ext}'
+    return None
 
 
 class ClothesSerializer(serializers.ModelSerializer):

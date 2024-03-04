66c62602b80b29f3fbd8ffce8d7f8b998ae25269
주기현 <0318joo@naver.com>
2024-03-04 16:41:48 +0900

    사용하지 않는 모듈 제거

--- server/user/serializers.py
+++ server/user/serializers.py
@@ -1,7 +1,6 @@
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import serializers, status
 from rest_framework_simplejwt.views import (
-    TokenBlacklistView,
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,

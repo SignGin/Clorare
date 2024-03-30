d2c460aa8ab3a774d24a9a45618cb10e07afa79c
주기현 <0318joo@naver.com>
2024-03-30 17:26:12 +0900

    clothes 모델 주석 제거

--- server/clothes/models.py
+++ server/clothes/models.py
@@ -18,10 +18,10 @@ class Clothes(models.Model):
         ('autumn', '가을'),
         ('winter', '겨울')
     ]
-    category = models.CharField(max_length=6, choices=CATEGORY)  # ex) 상의, 하의
-    cloth_type = models.CharField(max_length=20)  # ex) 티셔츠, 니트
-    season = models.CharField(max_length=6, choices=SEASON)  # ex) 봄, 여름, 가을, 겨울
-    gender = models.CharField(max_length=6, choices=GENDER)  # 여성, 남성, 무관
+    category = models.CharField(max_length=6, choices=CATEGORY)
+    cloth_type = models.CharField(max_length=20)
+    season = models.CharField(max_length=6, choices=SEASON)
+    gender = models.CharField(max_length=6, choices=GENDER)
     image = models.ImageField(default="default_image.jpg", null=True, blank=True)
 
     def __str__(self):
